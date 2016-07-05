ca3c4033133f9b364a3f270f87b3ef218fee47bc
Mark Alston <malston@pivotal.io>
2016-07-05 12:15:44 -0600

    Remove datianshi/gradle usage

--- ci/build-artifact.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-export GRADLE_OPTS=-Dorg.gradle.native=false
-version=`cat version/number`
-cd sample-spring-cloud-svc-repo
-#echo $version
-gradle assemble -Pversion=$version
-#ls build/libs/
--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -5,6 +5,6 @@ export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd sample-spring-cloud-svc-repo
 #echo $version
-gradle assemble -Pversion=$version
-mv build/libs/sample-spring-cloud-svc-ci-*.war ../build/
+./gradlew assemble -Pversion=$version
+mv build/libs/sample-spring-cloud-svc-ci-*.jar ../build/
 #ls build/libs/
