1774c38d9843206afb1740d132ef054b694d0a60
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-07-08 09:36:41 -0700

    Getting past - The ‘stage’ step must not be used inside a ‘parallel’ block.

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -22,18 +22,18 @@ def push(api, user, password, org, space, domain, hostname) {
 }
 
 def pushIf(api, user, password, org, space, domain, hostname) {
-    stage "cf push if acceptable"
+//    stage "cf push if acceptable"
     input "Deploy to ${org} ${space}?"
     sh "./gradlew cf-push -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 def runSmokeTests(url, user) {
-	stage 'run smoke tests'
+//	stage 'run smoke tests'
     sh "./gradlew cfSmokeTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 def runAcceptanceTests(url, user) {
-	stage 'run acceptance tests'
+//	stage 'run acceptance tests'
     sh "./gradlew cfAcceptanceTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
