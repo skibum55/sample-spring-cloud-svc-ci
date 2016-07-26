edfff109a5e8fa23e6cee9970014424c87ef4b3d
Mark Alston <malston@pivotal.io>
2016-07-26 07:09:05 -0400

    Add full-stacktrace option, etc to gradle cli

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -4,7 +4,6 @@ set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 cd sample-spring-cloud-svc-repo
-#echo $version
-./gradlew assemble -Pversion=$version
+echo "Version: $version"
+./gradlew --full-stacktrace --parallel --no-daemon assemble -Pversion=$version 
 mv build/libs/sample-spring-cloud-svc-ci-*.jar ../build/
-#ls build/libs/
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -3,5 +3,5 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd sample-spring-cloud-svc-repo
-./gradlew -v
 ./gradlew test
+./gradlew --full-stacktrace --parallel --no-daemon test
