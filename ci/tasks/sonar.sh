f7d8884276286ad7593604e5af76280abce0414d
Mark Alston <malston@pivotal.io>
2016-07-26 09:30:40 -0400

    Add sonar to quality stage

---
+++ ci/tasks/sonar.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+export GRADLE_OPTS=-Dorg.gradle.native=false
+cd sample-spring-cloud-svc-repo
+./gradlew --full-stacktrace --parallel --no-daemon sonarqube -Dsonar.host.url=${sonar-url} -Dsonar.verbose=true
