43721cd8662d99407cd65823c59ca3da98d4b7fa
Mark Alston <malston@pivotal.io>
2016-07-26 06:04:01 -0400

    Add --no-daemon option

--- ci/pipeline.groovy
+++ ci/pipeline.groovy
@@ -1,14 +1,14 @@
 def build() {
 //    def maven = docker.image("maven:3.3.3-jdk-8")
 //    maven.inside {
-        sh './gradlew --full-stacktrace build -x test'
+        sh './gradlew --full-stacktrace --parallel --no-daemon build -x test'
 //    }
 }
 
 def clean_test() {
 //    def maven = docker.image("maven:3.3.3-jdk-8")
 //    maven.inside {
-        sh './gradlew --full-stacktrace clean test assemble'
+        sh './gradlew --full-stacktrace --parallel --no-daemon clean test assemble'
         step([$class: 'JUnitResultArchiver', testResults: '**/build/test-results/TEST-*.xml'])
         dir('build/libs') {stash name: 'jar', includes: 'sample-spring-cloud-svc-*.jar'}
         // try {
@@ -29,22 +29,22 @@ def sonar(url) {
 
 def push(api, user, password, org, space, domain, hostname) {
     sh "ls -la"
-    sh "./gradlew --full-stacktrace cf-push -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
+    sh "./gradlew --full-stacktrace --no-daemon cf-push -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 def pushIf(api, user, password, org, space, domain, hostname) {
     input "Deploy to ${org} ${space}?"
-    sh "./gradlew --full-stacktrace cf-push -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
+    sh "./gradlew --full-stacktrace --no-daemon cf-push -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 def runSmokeTests(api, user, password, org, space, domain, hostname) {
     dir('build/libs') {unstash name:'jar'}
-    sh "./gradlew --full-stacktrace cfSmokeTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
+    sh "./gradlew --full-stacktrace --no-daemon cfSmokeTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 def runAcceptanceTests(api, user, password, org, space, domain, hostname) {
     dir('build/libs') {unstash name:'jar'}
-    sh "./gradlew --full-stacktrace cfAcceptanceTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
+    sh "./gradlew --full-stacktrace --no-daemon cfAcceptanceTest -Pcf.ccHost=${api} -Pcf.ccUser=${user} -Pcf.ccPassword=${password} -Pcf.org=${org} -Pcf.space=${space} -Pcf.domain=${domain} -Pcf.hostName=${hostname}"
 }
 
 return this;
