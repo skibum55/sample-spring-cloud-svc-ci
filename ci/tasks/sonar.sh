c01b99180234cc8a18ddc85ed1ac9401ddd6ae61
Mark Alston <malston@pivotal.io>
2016-07-26 14:03:56 -0400

    Build quality metrics

--- ci/tasks/sonar.sh
+++ ci/tasks/sonar.sh
@@ -3,4 +3,4 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd sample-spring-cloud-svc-repo
-./gradlew --full-stacktrace --parallel --no-daemon sonarqube -Dsonar.host.url=$SONAR_URL -Dsonar.verbose=true
+./gradlew --full-stacktrace --parallel --no-daemon build sonarqube -Dsonar.host.url=$SONAR_URL -Dsonar.verbose=true
